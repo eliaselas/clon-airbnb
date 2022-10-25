a798280a428d34650c790836bddba5548dc76b41
elias <hernandezelas@gmail.com>
2022-10-25 16:18:23 -0600

    Agregando estilos al archivo App.css y Navbar.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Navbar from './componentes/Navbar';
-import './App.css';
-
+import Navbar from "./componentes/Navbar";
+import "./App.css";
+import Hero from "./componentes/Hero";
+import Card from "./componentes/Card";
+import data from './data';
 function App() {
+
+  const cards = data.map((item) => {
+    return (
+      <Card key={item.id}
+        {...item}
+      />
+    )
+  })
+
+  console.log(cards)
   return (
     <div className="App">
       <Navbar />
-      App Componente
+      <Hero />
+      <div className="card--list">
+        {cards}
+      </div>
     </div>
   );
 }
